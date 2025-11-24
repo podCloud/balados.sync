ddccc9a3146f9017b43a8446254e6a2366a0062f
Pof Magicfingers <pof@podcloud.fr>
2025-11-24 18:12:43 +0100

    docs: add formatter configuration for markdown files
    Add markdown files to formatter inputs to enable code formatting
    in documentation files.

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["mix.exs", "config/*.exs"],
+  inputs: ["mix.exs", "config/*.exs", "*.md", "docs/**/*.{md,livemd}"],
   subdirectories: ["apps/*"]
 ]
